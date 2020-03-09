49817ea660440e1e71f72ff4fa07c17d3a42061d
calerz <18329776952@163.com>
2020-03-09 16:57:35 +0800

    @author caler @content

---
+++ src/main/java/com/jkq/jsqiang/entity/Foods.java
@@ -0,0 +1,93 @@
+package com.jkq.jsqiang.entity;
+
+public class Foods {
+    private Integer id;
+
+    private String foodname;
+
+    private String name;
+
+    private Double price;
+
+    private String description;
+
+    private Integer num;
+
+    private String picture;
+
+    private String state;
+
+    private String fenlei;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFoodname() {
+        return foodname;
+    }
+
+    public void setFoodname(String foodname) {
+        this.foodname = foodname == null ? null : foodname.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture == null ? null : picture.trim();
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state == null ? null : state.trim();
+    }
+
+    public String getFenlei() {
+        return fenlei;
+    }
+
+    public void setFenlei(String fenlei) {
+        this.fenlei = fenlei == null ? null : fenlei.trim();
+    }
+}
